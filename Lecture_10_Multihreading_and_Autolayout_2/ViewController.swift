a8ab87775709e78dfa2cdac4fc2af1babe111377
AdamWD <falco1016@gmail.com>
2018-01-19 13:10:22 +0800

    Initial Commit

---
+++ Lecture_10_Multihreading_and_Autolayout_2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lecture_10_Multihreading_and_Autolayout_2
+//
+//  Created by adamwang on 2018/1/19.
+//  Copyright © 2018年 adamwang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
